6cafb61381de241f55c101e10edbf5a52e7427e1
IGN-Styly <claudioresendeptbr@gmail.com>
2024-06-08 15:15:02 +0100

    NOW its fixed right?

--- media/caddy.nix
+++ media/caddy.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}:{
   services.caddy = {
     enable=true;
-    globalConfig = ''{
+    globalConfig = ''
 pki {
 ca master {
     name "T3 Labs CA"
     root_cn "T3 Labs CA - 2024 ECC Root"
-}}
+}
     '';
     virtualHosts."jellyfin.t3labs.io".extraConfig=''
     reverse_proxy http://192.168.122.37:8096
