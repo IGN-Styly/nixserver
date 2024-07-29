65d0a8aff476aa3e1827063e27a9a3d237768a8d
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-29 15:40:37 +0100

    bugz

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+    description = "NixServer Flake";
+
+    inputs= {
+        nixpkgs.url = "nixpkgs/nixos-24.05";
+    };
+
+    outputs = {self,nixpkgs,...}:
+    let
+        lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+        nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+        };
+    };
+    };
+}
