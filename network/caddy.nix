0079f6944ec44d4dbed33b350798fbe8d21a9208
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-29 21:45:28 +0100

    change domain

--- network/caddy.nix
+++ network/caddy.nix
@@ -6,7 +6,7 @@
 }: {
   services.caddy = {
     enable = true;
-    virtualHosts."nixos.local".extraConfig = ''
+    virtualHosts."nixos.lab".extraConfig = ''
       reverse_proxy http://192.168.122.37:7575
     '';
   };
