38352aca2a64dcf1c28c253280998642b7bdd810
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-29 20:15:53 +0100

    test2

--- network/homarr.nix
+++ network/homarr.nix
@@ -2,11 +2,11 @@
   virtualisation.oci-containers.containers.homarr = {
     image = "ghcr.io/ajnart/homarr:latest";
     volumes = [
-      "/home/docker/homarr:./homarr/configs:/app/data/configs"
-      "/home/docker/homarr:./homarr/icons:/app/public/icons"
-      "/home/docker/homarr:./homarr/data:/data"
+      "./homarr/configs:/app/data/configs"
+      "./homarr/icons:/app/public/icons"
+      "./homarr/data:/data"
     ];
-    ports=["7575:7575" "*:*"];
+    ports=["7575:7575"];
 
   };
 }
