1c1ebc1029312891aa2847e217620d7f192ea0dc
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-29 22:00:57 +0100

    fixed caddy cfg

--- network/caddy.nix
+++ network/caddy.nix
@@ -8,9 +8,7 @@
     enable = true;
     virtualHosts."nixos.lab".extraConfig = ''
       reverse_proxy http://192.168.122.37:7575
-    '';
-    globalConfig = ''
-    tls internal
+      tls internal
     '';
   };
 }
