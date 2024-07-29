68a3d2b6e553382156c7cbd2d9a2c238e0ac7891
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-29 22:51:05 +0100

    base domain redirects to dash

--- network/caddy.nix
+++ network/caddy.nix
@@ -7,12 +7,7 @@
   services.caddy = {
     enable = true;
     virtualHosts."nixos.lab".extraConfig = ''
-      forward_auth 192.168.122.37:9091 {
-        uri /api/verify?rd=auth.nixos.lab
-        copy_headers Remote-User Remote-Groups Remote-Name Remote-Email
-      }
-      tls internal
-      reverse_proxy http://192.168.122.37:7575
+      redir https://dash.{host}
         tls internal
     '';
     virtualHosts."auth.nixos.lab".extraConfig = ''
