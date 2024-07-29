a3f652cf46eaec34b015c464129b5b5a28a609d3
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-29 15:04:05 +0100

    i gave up on commit msgs

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-    "./hardware-configuration.nix"
+    ./hardware-configuration.nix
     ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # Use the systemd-boot EFI boot loader.
