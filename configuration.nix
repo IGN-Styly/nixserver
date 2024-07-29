4631c629a6e8149f8371e6ce7e2c8548a450f2ef
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-29 15:18:02 +0100

    bugz

--- configuration.nix
+++ configuration.nix
@@ -27,11 +27,11 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-     font = "Lat2-Terminus16";
-     keyMap = lib.mkForce "us";
-     useXkbConfig = true; # use xkb.options in tty.
-   };
+  # console = {
+  #    font = "Lat2-Terminus16";
+  #    keyMap = lib.mkForce "us";
+  #    useXkbConfig = true; # use xkb.options in tty.
+  #  };
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
