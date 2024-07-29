e079438ab6653576a5a6dce6f1fcba2b0c3c7260
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-30 00:32:46 +0100

    format +weird

--- network/authelia.nix
+++ network/authelia.nix
@@ -1,27 +1,34 @@
-{pkgs,inputs, ...}: {
-    imports = [ inputs.sops-nix.nixosModules.sops ];
-    sops.defaultSopsFile=./secrets/authelia.yaml;
-    sops.defaultSopsFormat="yaml";
-    sops.age.keyFile="/home/styly/.ssh/keys.txt";
-    sops.secrets."authelia/jwtSecret" = {owner = services.authelia.instances.main.name;};
-    sops.secrets."authelia/sessionKey"={owner = services.authelia.instances.main.name;};
-    sops.secrets."authelia/storageKey" = {
-    owner = services.authelia.instances.main.name;
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [inputs.sops-nix.nixosModules.sops];
+  sops.defaultSopsFile = ./secrets/authelia.yaml;
+  sops.defaultSopsFormat = "yaml";
+  sops.age.keyFile = "/home/styly/.ssh/keys.txt";
+  name = "main";
+  secretName = concatStrings ["authelia-" name];
+  sops.secrets."authelia/jwtSecret" = {owner = secretName;};
+  sops.secrets."authelia/sessionKey" = {owner = secretName;};
+  sops.secrets."authelia/storageKey" = {
+    owner = secretName;
+  };
+  services.authelia.instances.name = {
+    enable = true;
+    secrets = {
+      jwtSecretFile = /run/secrets/authelia/jwtSecret;
+      storageEncryptionKeyFile = /run/secrets/authelia/storageKey;
+      sessionSecretFile = /run/secrets/authelia/sessionKey;
     };
-    services.authelia.instances.main = {
-        enable=true;
-        secrets = {
-             jwtSecretFile = /run/secrets/authelia/jwtSecret;
-             storageEncryptionKeyFile = /run/secrets/authelia/storageKey;
-             sessionSecretFile = /run/secrets/authelia/sessionKey;
-           };
-           settings = {
-                theme = "dark";
-                default_redirection_url = "https://nixos.lab";
+    settings = {
+      theme = "dark";
+      default_redirection_url = "https://nixos.lab";
 
-                server = {
-                  host = "127.0.0.1";
-                  port = 9091;
-                };};
+      server = {
+        host = "127.0.0.1";
+        port = 9091;
+      };
     };
+  };
 }
