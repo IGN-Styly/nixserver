e24b49f542c8a4952be1aa898cff21c1dc2388c7
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-30 01:35:50 +0100

    fix attempt

--- network/authelia.nix
+++ network/authelia.nix
@@ -15,6 +15,7 @@
   };
   services.authelia.instances.main = {
     enable = true;
+    secrets.manual=true;
     secrets = {
       jwtSecretFile = "${config.sops.secrets."jwtSecret".path}";
       storageEncryptionKeyFile = "${config.sops.secrets."sessionKey".path}";
