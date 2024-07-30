301517e162cf07ebffc0e476752d315b2a35ce2c
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-30 21:26:00 +0100

    better cfg

--- network/authelia.nix
+++ network/authelia.nix
@@ -21,7 +21,24 @@
       server.endpoints.authz.forward-auth.implementation = "ForwardAuth";
       theme = "dark";
       default_redirection_url = "https://nixos.lab";
-
+      access_control = {
+        default_policy = "deny";
+        rules = [
+          {
+            domain = "*.nixos.lab";
+            policy = "one_factor";
+          }
+        ];
+      };
+      session = {
+        cookies = [
+          {
+            domain = "nixos.lab";
+            authelia_url = "https://auth.nixos.lab";
+          }
+        ];
+      };
+      #settingsFiles = [ ./oidc_clients.yaml ];
       server = {
         host = "127.0.0.1";
         port = 9091;
