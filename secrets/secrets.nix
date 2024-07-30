a7f7de94c5f236e2b43803933b5ae141da7c16c4
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-30 20:13:07 +0100

    bug fix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -16,21 +16,23 @@
       authelia = {
         jwtSecretFile = {
           description = "path to the jwtsecret file";
-          default = inputs.sops.nixosModules.sops.secrets."jwtSecret".path;
+          default = "/";
           type = lib.types.path;
         };
         sessionSecretFile = {
           description = "path to the jwtsecret file";
-          default = inputs.sops.nixosModules.sops.secrets."sessionKey".path;
+          default = "/";
           type = lib.types.path;
         };
-
         storageEncryptionKeyFile = {
           description = "path to the jwtsecret file";
-          default = inputs.sops.nixosModules.sops.secrets."storageKey".path;
+          default = "/";
           type = lib.types.path;
         };
       };
     };
   };
+  config.secrets.authelia.jwtSecretFile = inputs.sops.nixosModules.sops.secrets."jwtSecret".path;
+  config.secrets.authelia.sessionSecretFile = inputs.sops.nixosModules.sops.secrets."sessionKey".path;
+  config.secrets.authelia.storageEncryptionKeyFile = inputs.sops.nixosModules.sops.secrets."storageKey".path;
 }
