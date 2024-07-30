302d9029591421d1a6d5176e0d77ea5c022b8aaf
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-30 20:47:32 +0100

    fixed secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,13 +6,15 @@
 }: {
   imports = [inputs.sops-nix.nixosModules.sops];
   config = {
-    defaultSopsFile = "./secrets.yaml";
-    defaultSopsFormat = "yaml";
-    age = {
-      # This is the default AGE key that will be used for bootstrapping the system
-      keyFile = "/root/age/age-key.txt";
-      # This will automatically import SSH host keys as AGE keys
-      # sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    sops = {
+      defaultSopsFile = "./secrets.yaml";
+      defaultSopsFormat = "yaml";
+      age = {
+        # This is the default AGE key that will be used for bootstrapping the system
+        keyFile = "/root/age/age-key.txt";
+        # This will automatically import SSH host keys as AGE keys
+        # sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      };
     };
   };
 }
