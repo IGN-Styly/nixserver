283f68e7989d76022f2a30f5ecda64c5b089192a
IGN-Styly <claudioresendeptbr@gmail.com>
2024-07-30 15:17:50 +0100

    fix boot issues

--- network/authelia.nix
+++ network/authelia.nix
@@ -7,7 +7,7 @@
   imports = [inputs.sops-nix.nixosModules.sops];
   sops.defaultSopsFile = ../secrets/authelia.yaml;
   sops.defaultSopsFormat = "yaml";
-  sops.age.keyFile = "/home/styly/.ssh/keys.txt";
+  sops.age.keyFile = "/root/age/keys.txt";
   sops.secrets."jwtSecret" = {owner = "authelia-main";};
   sops.secrets."sessionKey" = {owner = "authelia-main";};
   sops.secrets."storageKey" = {
