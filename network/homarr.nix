e325c1837ea6d53ef65cb8f310bf7aa64900fbb2
IGN-Styly <claudioresendeptbr@gmail.com>
2025-02-02 19:11:11 +0000

    wipe

--- network/homarr.nix
+++ network/homarr.nix
@@ -1,11 +1,1 @@
-{pkgs, ...}: {
-  virtualisation.oci-containers.containers.homarr = {
-    image = "ghcr.io/ajnart/homarr:latest";
-    volumes = [
-      "./homarr/configs:/app/data/configs"
-      "./homarr/icons:/app/public/icons"
-      "./homarr/data:/data"
-    ];
-    ports = ["7575:7575"];
-  };
-}
+
