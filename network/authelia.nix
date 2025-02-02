a41cff51a1a25477c6b81359627f5afa00563eec
Claudio Torres <claudiotorresptpt@gmail.com>
2025-02-02 20:38:32 +0000

    smthg

--- network/authelia.nix
+++ network/authelia.nix
@@ -1,1 +1,17 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    authelia
+  ];
+  services.authelia.main.secrets.jwtSecretFile = config.sops.secrets."jwtSecret".path;
+  services.authelia.main.secrets.storageEncryptionKeyFile = config.sops.secrets."cryptKey".path;
+  services.authelia.instances.main = {
+    enable = true;
 
+  };
+}
